5c9aa3728e195b12b7f4497406dfea20c6539363
Carlo Barraco <carlobarraco@gmail.com>
2015-06-13 19:06:16 -0400

    Made API calls from the WaterlooApi class super generic and extendable. also added event API call

--- MapApp/app/src/main/java/ca/uwaterloo/mapapp/logic/net/WaterlooApi.java
+++ MapApp/app/src/main/java/ca/uwaterloo/mapapp/logic/net/WaterlooApi.java
@@ -7,11 +7,12 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import ca.uwaterloo.mapapp.logic.Logger;
-import ca.uwaterloo.mapapp.logic.net.objects.Building;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
 
@@ -20,40 +21,50 @@ import retrofit.converter.GsonConverter;
  * 6/11/15
  */
 public class WaterlooApi {
-    public static final String ACTION_GOT_BUILDINGS = "ca.uwaterloo.mapapp.logic.net.WaterlooApi.ACTION_GOT_BUILDINGS";
-    public static final String EXTRA_BUILDINGS = "ca.uwaterloo.mapapp.logic.net.WaterlooApi.EXTRA_BUILDINGS";
-    public static Gson gson = new GsonBuilder()
+    public static final String INTENT_PREFIX = "ca.uwaterloo.mapapp.logic.net.WaterlooApi.";
+    public static final String ACTION_GOT_LIST = INTENT_PREFIX + "ACTION_GOT_LIST";
+    public static final String EXTRA_LIST = INTENT_PREFIX + "EXTRA_LIST";
+    public static final String EXTRA_CLASS = INTENT_PREFIX + "EXTRA_CLASS";
+
+    private static Gson gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-            .registerTypeAdapter(Building.class, new WaterlooApiJsonDeserializer<Building>())
             .registerTypeAdapter(List.class, new WaterlooApiJsonDeserializer<List>())
             .create();
     private static RestAdapter restAdapter = new RestAdapter.Builder()
             .setEndpoint(WaterlooApiRestService.API_ENDPOINT)
             .setConverter(new GsonConverter(gson))
             .build();
-    public static WaterlooApiRestService service = restAdapter.create(WaterlooApiRestService.class);
+    private static WaterlooApiRestService service = restAdapter.create(WaterlooApiRestService.class);
 
-    public static void requestBuildings(final Context context) {
+    public static void requestList(final Context context, final Class clazz) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                long startTime = System.currentTimeMillis();
-                List<Building> result = null;
+                final long startTime = System.currentTimeMillis();
+                final String className = clazz.getSimpleName();
                 try {
-                    result = service.getBuildings();
+                    final List result = invokeApiMethodFromClassName(className);
+                    final long duration = System.currentTimeMillis() - startTime;
+                    Logger.info("Got list of %ss from API in %dms", className, duration);
+                    broadcastGotList(context, result, clazz);
                 } catch (Exception e) {
-                    Logger.error("Failed to get Buildings from API", e);
+                    Logger.error("Failed to get list of %ss from API", e, className);
                 }
-                long duration = System.currentTimeMillis() - startTime;
-                Logger.info("Got Buildings from API in %dms", duration);
-                broadcastBuildingsProcessed(context, result);
+
             }
         }).start();
     }
 
-    private static void broadcastBuildingsProcessed(Context context, List<Building> buildings) {
-        Intent buildingsIntent = new Intent(ACTION_GOT_BUILDINGS);
-        buildingsIntent.putExtra(EXTRA_BUILDINGS, new ArrayList<>(buildings));
+    private static List invokeApiMethodFromClassName(String clazzSimpleName) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        String format = String.format("get%ss", clazzSimpleName);
+        Method method = service.getClass().getMethod(format);
+        return (List) method.invoke(service);
+    }
+
+    private static void broadcastGotList(Context context, List list, Class clazz) {
+        Intent buildingsIntent = new Intent(ACTION_GOT_LIST);
+        buildingsIntent.putExtra(EXTRA_CLASS, clazz.getSimpleName());
+        buildingsIntent.putExtra(EXTRA_LIST, new ArrayList<>(list));
         context.sendBroadcast(buildingsIntent);
     }
 
