2b80205fc131816b726c0cc1d669bd885d5fdbc8
Carlo Barraco <carlobarraco@gmail.com>
2015-06-13 18:56:58 -0400

    Split up course into separate files

---
+++ MapApp/app/src/main/java/ca/uwaterloo/mapapp/logic/net/objects/course/Course.java
@@ -0,0 +1,14 @@
+package ca.uwaterloo.mapapp.logic.net.objects.course;
+
+/**
+ * Created by Hyunwook on 2015-06-13.
+ */
+
+import java.util.List;
+
+public class Course {
+    private String subject;
+    private String title;
+    private double classNumber;
+    private List<Class> classes;
+}
---
+++ MapApp/app/src/main/java/ca/uwaterloo/mapapp/logic/net/objects/course/Dates.java
@@ -0,0 +1,11 @@
+package ca.uwaterloo.mapapp.logic.net.objects.course;
+
+/**
+ * Created by cjbarrac
+ * 6/13/15
+ */
+public class Dates {
+    private String startTime;
+    private String endTime;
+    private String weekdays;
+}
