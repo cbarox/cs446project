c77789397b8131a34b62a3f6dcfac37af5cfe54c
Carlo Barraco <carlobarraco@gmail.com>
2015-06-14 00:20:30 -0400

    Add broadcast receiver to new notes activity for handling getting buildings list

--- MapApp/app/src/main/java/ca/uwaterloo/mapapp/ui/NewNoteActivity.java
+++ MapApp/app/src/main/java/ca/uwaterloo/mapapp/ui/NewNoteActivity.java
@@ -1,5 +1,9 @@
 package ca.uwaterloo.mapapp.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,14 +12,26 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import ca.uwaterloo.mapapp.R;
 import ca.uwaterloo.mapapp.data.DataManager;
 import ca.uwaterloo.mapapp.data.DatabaseHelper;
 import ca.uwaterloo.mapapp.data.objects.Note;
+import ca.uwaterloo.mapapp.logic.Logger;
+import ca.uwaterloo.mapapp.logic.net.WaterlooApi;
+import ca.uwaterloo.mapapp.logic.net.objects.Building;
 
 public class NewNoteActivity extends ActionBarActivity {
+
+    /**
+     * All the actions that are processed by the broadcast receiver
+     */
+    private static final String[] receiverActions = {
+            WaterlooApi.ACTION_GOT_LIST
+    };
 
     @InjectView(R.id.tool_bar)
     protected Toolbar mToolbar;
@@ -24,6 +40,39 @@ public class NewNoteActivity extends ActionBarActivity {
     protected EditText mTitle;
     @InjectView(R.id.note_desc)
     protected EditText mDescription;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Bundle extras = intent.getExtras();
+            switch (action) {
+                case WaterlooApi.ACTION_GOT_LIST: {
+                    if (extras == null) {
+                        Logger.error("No extras supplied from intent %s", WaterlooApi.ACTION_GOT_LIST);
+                        return;
+                    }
+                    List list = (List) extras.get(WaterlooApi.EXTRA_LIST);
+                    if (list == null) {
+                        Logger.error("Couldn't get list from intent extras");
+                        return;
+                    }
+                    String className = extras.getString(WaterlooApi.EXTRA_CLASS);
+                    if (className == null) {
+                        Logger.error("Couldn't get class name from intent extras");
+                        return;
+                    }
+                    if (className.equals("Building")) {
+                        handleGotBuildings(list);
+                    }
+                }
+            }
+        }
+    };
+
+    private void handleGotBuildings(List<Building> buildings) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +90,20 @@ public class NewNoteActivity extends ActionBarActivity {
             }
         });
 
+        // Register the broadcast receiver
+        IntentFilter intentFilter = new IntentFilter();
+        for (String action : receiverActions) {
+            intentFilter.addAction(action);
+        }
+        registerReceiver(broadcastReceiver, intentFilter);
+
+        WaterlooApi.requestList(this, Building.class);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
     }
 
     @Override
@@ -60,7 +123,7 @@ public class NewNoteActivity extends ActionBarActivity {
             Note note = new Note();
             note.setTitle(title);
             note.setDescription(description);
-            // TODO get building code from intent
+            // TODO get building code from dropdown
             //note.setBuildingCode(buildingCode);
 
             // Insert note into database
