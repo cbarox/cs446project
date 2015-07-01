0ced4761eaddc7d9741036be3fe0f8c81455830c
Carlo Barraco <carlobarraco@gmail.com>
2015-07-01 16:28:43 -0400

    Changed JSoup to use callbacks and added event note class

---
+++ MapApp/server/src/main/java/ca/uwaterloo/mapapp/server/logic/net/JsoupApi.java
@@ -0,0 +1,41 @@
+package ca.uwaterloo.mapapp.server.logic.net;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javafx.util.Callback;
+
+/**
+ * Created by Hyunwook on 2015-06-14.
+ */
+public class JsoupApi implements Runnable {
+
+    private String url;
+    private String selector;
+    private Callback callback;
+
+    /**
+     * @param url      The url to get the data from
+     * @param selector The selector to
+     * @param callback
+     */
+    public JsoupApi(String url, String selector, Callback callback) {
+        this.url = url;
+        this.selector = selector;
+        this.callback = callback;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element element = doc.select(selector/*e.g. span.fn*/).first();
+            callback.call(element.toString());
+        } catch (Exception e) {
+            callback.call(null);
+        }
+    }
+}
+
+
