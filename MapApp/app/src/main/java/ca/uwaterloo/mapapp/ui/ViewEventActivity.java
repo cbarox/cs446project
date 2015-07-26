b8737f31ef4914c16b7fe5ed91b62fbb8a964085
cjbarrac <carlobarraco@gmail.com>
2015-07-26 19:00:29 -0400

    forgot to call loadEventImages

--- MapApp/app/src/main/java/ca/uwaterloo/mapapp/ui/ViewEventActivity.java
+++ MapApp/app/src/main/java/ca/uwaterloo/mapapp/ui/ViewEventActivity.java
@@ -123,6 +123,7 @@ public class ViewEventActivity extends ActionBarActivity {
         loadEventNotes();
         loadEventTimes();
         loadEventRankings();
+        loadEventImages();
     }
 
     private void loadEventNotes() {
