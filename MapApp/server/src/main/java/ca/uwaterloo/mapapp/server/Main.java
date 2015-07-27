d6c626b02c47a8fea696ac37c4566d9eb2c524e8
cjbarrac <carlobarraco@gmail.com>
2015-07-27 01:22:53 -0400

    created image table

--- MapApp/server/src/main/java/ca/uwaterloo/mapapp/server/Main.java
+++ MapApp/server/src/main/java/ca/uwaterloo/mapapp/server/Main.java
@@ -23,6 +23,7 @@ import ca.uwaterloo.mapapp.server.logic.net.RankingRoute;
 import ca.uwaterloo.mapapp.shared.data.DataManager;
 import ca.uwaterloo.mapapp.shared.objects.building.Building;
 import ca.uwaterloo.mapapp.shared.objects.event.Event;
+import ca.uwaterloo.mapapp.shared.objects.event.EventImage;
 import ca.uwaterloo.mapapp.shared.objects.event.EventNote;
 import ca.uwaterloo.mapapp.shared.objects.event.EventRanking;
 import ca.uwaterloo.mapapp.shared.objects.event.EventTimes;
@@ -62,7 +63,7 @@ public class Main {
             MagicLogger.log("Creating database tables");
             TableUtils.createTableIfNotExists(connectionSource, EventNote.class);
             TableUtils.createTableIfNotExists(connectionSource, EventRanking.class);
-            //TableUtils.createTableIfNotExists(connectionSource, EventImage.class);
+            TableUtils.createTableIfNotExists(connectionSource, EventImage.class);
             TableUtils.createTableIfNotExists(connectionSource, Building.class);
             TableUtils.createTableIfNotExists(connectionSource, Event.class);
             buildingDataUpdater = new BuildingDataUpdater();
